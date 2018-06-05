1f3e69b98323efc85611fca5e04eda7451d2e98c
sfilippo <filipposoldati7@gmail.com>
2018-06-05 12:51:05 +0300

    Update sample with values

--- samples/values.f
+++ samples/values.f
@@ -4,4 +4,4 @@ r :rational is -7\3;
 c :complex is -5.3i0.0;
 s :string is "Hello world!\n";
 a :string is "I don't like \"Hello world!\\n\" message\n\tBut I guess I'll have to live with them.\a";
-//z :[{(integer, integer) : (integer, integer)}] is [{(0, 10) : (0, -10), (10, 20) : (-10, -20)}, {(100, 1000) : (300, 3000)}];
+z :[{(integer, integer) : (integer, integer)}] is [{(0, 10) : (0, -10), (10, 20) : (-10, -20)}, {(100, 1000) : (300, 3000)}];
