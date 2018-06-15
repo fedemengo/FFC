ab11812decab4e76f55ba269cca926fe9db55cd9
Filippo Soldati <filipposoldati7@gmail.com>
2018-06-15 14:37:45 +0300

    Add string support

--- FRunTime/FString.cs
+++ FRunTime/FString.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+
 namespace FFC.FRunTime
 {
     public class FString : FRTType
     {
-        
+        public string Text {get; set;}
+        public FString(string s)
+        {
+            Text = s;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
     }
 }
--- samples/prints.f
+++ samples/prints.f
@@ -3,33 +3,40 @@
     at the end of each 'il function'
 
 main is func() do
-    //basic int
+    print "basic int";
     print 3;
-    
-    //int expressions
+    print "";
+
+    print "int expressions";
     print 3 * 2;
-    
-    //other types
+    print "";
+
+    print "other types";
     print 1.0i-2.0;
-    
-    //int arrays
+    print "";
+
+    print "int arrays";
     print [1];
     print [3, 4];
-    
-    //any type arrays
+    print "";
+
+    print "any type arrays";
     print [2.4];
     print [[1, 2], [3, 4]];
-    
-    //arrays concatenation
+    print "";
+
+    print "arrays concatenation";
     print [3] + [3];
     print [[1, 2], [3, 4]] + [[5, 6], [7, 8]];
     print [[[1]]] + [[[1]]] + [[[1, 2],[3],[4, 5, 6]]];
+    print "";
 
-    //arrays array additions
+    print "arrays additions";
     print [3] + 4;
     print [1,3] + ([1, 2] + 4);
     print [1.3] + 2.4 + 2.3;
     print [[1]] + [1];
     print [[1]] + [0] + [[1], [0]] + [1];
+    print "";
 
 end
