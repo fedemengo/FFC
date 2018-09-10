b5e30597992cbdf3eafcb48611344102ca9d6bd3
Filippo Soldati <filipposoldati7@gmail.com>
2018-09-10 16:55:29 +0200

    Fix mergesort sample

--- samples/mergesort.f
+++ samples/mergesort.f
@@ -19,10 +19,10 @@ merge is func(a: [integer], b: [integer]) do
 		v := v + b[j];
 		j := j + 1;
 	end
+	return v;
 end
 
-// commento
-
+// returns two arrays [0, pos-1][pos, length-1]
 split is func(v: [integer], pos: integer) do
 	i is 0;
 	a: [integer] is [];
@@ -38,15 +38,17 @@ split is func(v: [integer], pos: integer) do
 	return (a, b);
 end
 
-
 mergesort is func(v: [integer]) do
+	if length(v) = 1 then
+		return v;
+	end
 	parts is split(v, round(length(v)/2));
-	return merge(parts.1, parts.2);
+	return merge(mergesort(parts.1), mergesort(parts.2));
 end
 
 main is func() do
 	v is [3, 5, 1, 2, 4, -2, 8];
 	s is mergesort(v);
-	for i in s loop print i; end
+	print s;
 end
 
